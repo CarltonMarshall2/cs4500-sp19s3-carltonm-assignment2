211e08635b88cecfa49e815e6800db41094a934a
Carlton Marshall II <carltonm@ccs.neu.edu>
2019-01-24 00:42:57 -0500

    added findUserById method

--- src/main/java/edu/neu/cs4500/services/UserService.java
+++ src/main/java/edu/neu/cs4500/services/UserService.java
@@ -22,4 +22,14 @@ public class UserService {
     public List<User> findAllUsers() {
         return users;
     }
+
+    @GetMapping("/api/user/{userId}")
+    public User findUserById(@PathVariable("userId") Integer id) {
+        for (User user : users) {
+            if (Objects.equals(user.getId(), id)) {
+                return user;
+            }
+        }
+        return null;
+    }
 }
