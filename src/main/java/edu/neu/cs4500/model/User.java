6b4eeb61055f5279c80d3dbda0a25ce0d7cf587a
Carlton Marshall II <carltonm@ccs.neu.edu>
2019-01-24 00:41:23 -0500

    added user class

---
+++ src/main/java/edu/neu/cs4500/model/User.java
@@ -0,0 +1,58 @@
+package edu.neu.cs4500.model;
+
+
+public class User {
+    private Integer id;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+
+    public User(Integer id, String username, String password, String firstName, String lastName) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
